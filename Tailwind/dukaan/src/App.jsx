9f60e4ccc4124e23c04cf060fff6a39b4509eb49
Adwaita Raj Modak <adwaitaraj@gmail.com>
2024-08-12 20:24:24 +0530

    dukaan

--- Tailwind/dukaan/src/App.jsx
+++ Tailwind/dukaan/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Navbar from './components/Navbar'
+import PayoutCard from './components/PayoutCard'
 import RevenueCard from './components/RevenueCard'
 import Sidebar from './components/Sidebar'
 import './index.css'
@@ -9,11 +10,37 @@ function App() {
   return (
     <>
       {/* <Navbar /> */}
-      {/* <Sidebar /> */}
-      <div className='grid grid-cols-3'>
+      <div>
 
-        <RevenueCard title={"Amount Pending"} amount={92000.12} countOrder={13} />
-        <RevenueCard title={"Amount Processed"} amount={9865487.12} />
+
+        <div className=''>
+          <Sidebar />
+
+        </div>
+
+
+
+        <div className='grid grid-cols-3 gap-4 mr-10 ml-60'>
+
+          <div>
+
+            <PayoutCard title="Next Payout" amount={2312.23} countOrder={23} nextPayment={"Today, 4:00 PM"} />
+          </div>
+          <div>
+            <RevenueCard title={"Amount Pending"} amount={92000.12} countOrder={13} />
+
+          </div>
+          <div>
+            <RevenueCard title={"Amount Processed"} amount={9865487.12} />
+
+          </div>
+
+          {/* <div className='grid grid-cols-1'>
+
+        </div> */}
+
+
+        </div>
       </div>
 
     </>
--- Tailwind/dukaan/src/components/Sidebar.jsx
+++ Tailwind/dukaan/src/components/Sidebar.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
+import img from '../assets/logo.png'
 
 const Sidebar = () => {
     return (
         <>
+            <div className="w-1/6 bg-[#1E2640] text-white p-2">
+                <div className=''>
+                    <div className='flex items-center justify-start'>
+                        <img className='rounded-full object-cover h-10 w-10' src={img} alt="Profile" />
+                        <div className='flex-grow ml-4'>
+                            Adwaita
+                            <div className='text-white underline'>
+                                <a href="/">Link</a>
+                            </div>
+                        </div>
+                        <div className=''>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
 
-            <div className="w-20 bg-blue-600 h-50">
-                
-
+                <div className=''>
+                    <ul>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                    </ul>
+                </div>
             </div>
-
         </>
     )
 }
