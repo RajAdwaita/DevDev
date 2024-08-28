a46028ca03bce239b4a7cf1f80b1e9cddb035187
Adwaita Raj Modak <adwaitaraj@gmail.com>
2024-08-28 16:00:27 +0530

    Done with prisma ORM

---
+++ prisma/src/index.ts
@@ -0,0 +1,75 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function createUser(username: string, password: string, firstName: string, lastName: string) {
+
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                username: username,
+                password: password,
+                firstName: firstName,
+                lastName: lastName,
+            },
+            select: {
+                id: true,
+                username: true
+            }
+        })
+        console.log(user);
+
+
+
+    }
+    catch (error) {
+        console.log(error);
+
+    }
+}
+
+interface UpdateParams {
+    firstName: string,
+    lastName: string
+}
+
+async function updateUser(username: string, { firstName, lastName }: UpdateParams) {
+    try {
+        const user = await prisma.user.update({
+            where: {
+                username: username,
+            },
+            data: {
+                firstName: firstName,
+                lastName: lastName,
+            },
+            select: {
+                firstName: true,
+                lastName: true
+            }
+        })
+
+        console.log(user);
+
+    }
+    catch (err) {
+        console.log(err);
+
+    }
+}
+updateUser('cr7',
+    {
+        firstName: 'Cristiano',
+        lastName: "Ronaldo"
+    }
+).then(() => {
+    console.log('User Updated');
+
+}).catch((e) => {
+    console.log(e);
+
+}).finally(async () => {
+    await prisma.$disconnect();
+})
+// createUser('lm10', 'abc123', 'Lewo', 'massi')
