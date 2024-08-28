efb2bcfeaca4740b3faf4647b704881e1e27a25c
Adwaita Raj Modak <adwaitaraj@gmail.com>
2024-08-28 20:04:08 +0530

    todo-prism

--- prisma/dist/index.js
+++ prisma/dist/index.js
@@ -1,69 +1,62 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
-function createUser(username, password, firstName, lastName) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const user = yield prisma.user.create({
-                data: {
-                    username: username,
-                    password: password,
-                    firstName: firstName,
-                    lastName: lastName,
-                },
-                select: {
-                    id: true,
-                    username: true
-                }
-            });
-            console.log(user);
-        }
-        catch (error) {
-            console.log(error);
-        }
-    });
-}
-function updateUser(username_1, _a) {
-    return __awaiter(this, arguments, void 0, function* (username, { firstName, lastName }) {
-        try {
-            const user = yield prisma.user.update({
-                where: {
-                    username: username,
-                },
-                data: {
-                    firstName: firstName,
-                    lastName: lastName,
-                },
-                select: {
-                    firstName: true,
-                    lastName: true
-                }
-            });
-            console.log(user);
-        }
-        catch (err) {
-            console.log(err);
-        }
-    });
-}
-updateUser('cr7', {
-    firstName: 'Cristiano',
-    lastName: "Ronaldo"
-}).then(() => {
-    console.log('User Updated');
-}).catch((e) => {
-    console.log(e);
-}).finally(() => __awaiter(void 0, void 0, void 0, function* () {
-    yield prisma.$disconnect();
-}));
+// async function createUser(username: string, password: string, firstName: string, lastName: string) {
+//     try {
+//         const user = await prisma.user.create({
+//             data: {
+//                 username: username,
+//                 password: password,
+//                 firstName: firstName,
+//                 lastName: lastName,
+//             },
+//             select: {
+//                 id: true,
+//                 username: true
+//             }
+//         })
+//         console.log(user);
+//     }
+//     catch (error) {
+//         console.log(error);
+//     }
+// }
+// interface UpdateParams {
+//     firstName: string,
+//     lastName: string
+// }
+// async function updateUser(username: string, { firstName, lastName }: UpdateParams) {
+//     try {
+//         const user = await prisma.user.update({
+//             where: {
+//                 username: username,
+//             },
+//             data: {
+//                 firstName: firstName,
+//                 lastName: lastName,
+//             },
+//             select: {
+//                 firstName: true,
+//                 lastName: true
+//             }
+//         })
+//         console.log(user);
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+// updateUser('cr7',
+//     {
+//         firstName: 'Cristiano',
+//         lastName: "Ronaldo"
+//     }
+// ).then(() => {
+//     console.log('User Updated');
+// }).catch((e) => {
+//     console.log(e);
+// }).finally(async () => {
+//     await prisma.$disconnect();
+// })
 // createUser('lm10', 'abc123', 'Lewo', 'massi')
--- prisma/src/index.ts
+++ prisma/src/index.ts
@@ -2,74 +2,82 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function createUser(username: string, password: string, firstName: string, lastName: string) {
+// async function createUser(username: string, password: string, firstName: string, lastName: string) {
 
 
-    try {
-        const user = await prisma.user.create({
-            data: {
-                username: username,
-                password: password,
-                firstName: firstName,
-                lastName: lastName,
-            },
-            select: {
-                id: true,
-                username: true
-            }
-        })
-        console.log(user);
+//     try {
+//         const user = await prisma.user.create({
+//             data: {
+//                 username: username,
+//                 password: password,
+//                 firstName: firstName,
+//                 lastName: lastName,
+//             },
+//             select: {
+//                 id: true,
+//                 username: true
+//             }
+//         })
+//         console.log(user);
 
 
 
-    }
-    catch (error) {
-        console.log(error);
+//     }
+//     catch (error) {
+//         console.log(error);
 
-    }
-}
+//     }
+// }
 
-interface UpdateParams {
-    firstName: string,
-    lastName: string
-}
+// interface UpdateParams {
+//     firstName: string,
+//     lastName: string
+// }
 
-async function updateUser(username: string, { firstName, lastName }: UpdateParams) {
-    try {
-        const user = await prisma.user.update({
-            where: {
-                username: username,
-            },
-            data: {
-                firstName: firstName,
-                lastName: lastName,
-            },
-            select: {
-                firstName: true,
-                lastName: true
-            }
-        })
+// async function updateUser(username: string, { firstName, lastName }: UpdateParams) {
+//     try {
+//         const user = await prisma.user.update({
+//             where: {
+//                 username: username,
+//             },
+//             data: {
+//                 firstName: firstName,
+//                 lastName: lastName,
+//             },
+//             select: {
+//                 firstName: true,
+//                 lastName: true
+//             }
+//         })
 
-        console.log(user);
+//         console.log(user);
 
-    }
-    catch (err) {
-        console.log(err);
+//     }
+//     catch (err) {
+//         console.log(err);
 
-    }
-}
-updateUser('cr7',
-    {
-        firstName: 'Cristiano',
-        lastName: "Ronaldo"
-    }
-).then(() => {
-    console.log('User Updated');
+//     }
+// }
+// updateUser('cr7',
+//     {
+//         firstName: 'Cristiano',
+//         lastName: "Ronaldo"
+//     }
+// ).then(() => {
+//     console.log('User Updated');
 
-}).catch((e) => {
-    console.log(e);
+// }).catch((e) => {
+//     console.log(e);
 
-}).finally(async () => {
-    await prisma.$disconnect();
-})
+// }).finally(async () => {
+//     await prisma.$disconnect();
+// })
 // createUser('lm10', 'abc123', 'Lewo', 'massi')
+
+
+const signup = (() => {
+    return (
+        <div>
+        </div>
+    )
+})
